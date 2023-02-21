cc7d5ae0b8c2914a5132a4f4b75dde1931217293
vmarc <vmarcial8209@gmail.com>
2023-02-20 21:12:39 -0800

    first commit

---
+++ src/main/java/com/vmarcialtest/helloworld/HelloController.java
@@ -0,0 +1,24 @@
+package com.vmarcialtest.helloworld;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//http://localhost:8080/MeepSunA3
+public class HelloController {
+
+    @RequestMapping("/MeepSunA3")
+     
+    
+  @GetMapping  
+    public String getName(@RequestParam (value = "name",defaultValue = "world" )String name  ){
+
+    return "hello  " + name  ;
+
+}
+
+
+}
---
+++ src/test/java/com/vmarcialtest/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmarcialtest.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
