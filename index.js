197c64968604f0e73f023abbe39f6ad842f6a9b0
vmarc <vmarcial8209@gmail.com>
2023-02-27 10:50:16 -0800

    removed something i didnt need in index

--- index.js
+++ index.js
@@ -24,18 +24,11 @@ app.get('/',(req,res) =>{res.sendStatus(200)})
 app.post('/', (req,res)=>{ 
    
      (console.log(JSON.stringify(req.body )))
-    // console.log(JSON.stringify({ x: 5, y: 6 }));
+    // console.log(JSON.stringify({ x: 5, y: 6 })); convert javascript to JSON
     
 
 })
 
-app.post('/', (req,res)=>{ // res.send(req.body.foodGenre)
-    //
-     (console.log('foodGenre: ' +req.body.foodGenre ))
-    // res.send(req.body.foodGenre)
-   //  (console.log(req.body.foodGenre))
-
-})
 
 
 
